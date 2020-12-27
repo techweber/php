905d75bd81fb51ce462b0757a0b805f0da60fb66
Usman <umahmood@gmail.com>
2020-12-27 19:32:47 +0500

    Add files via upload

---
+++ php-syntax-2.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+	<h1>My first PHP Page</h1>
+	
+	<?php
+		$color = 'red';
+		echo "My car is " . $color . "<br/>";
+		echo "My house is " . $COLOR . "<br/>";
+		echo "My boat is " . $coLOR . "<br/>";
+	?>
+
+</body>
+</html>
---
+++ php-syntax.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+	<h1>My first PHP Page</h1>
+	
+	<?php
+	// This is a single line comment
+	# This is also a single line comment
+	?>
+
+	<?php
+	/* 
+		This is a multi-line comment
+		This is a multi-line comment
+		This is a multi-line comment
+	*/
+	?>
+
+	<?php
+		$x = 5 /* + 15 */ + 5;
+	?>
+
+
+</body>
+</html>
