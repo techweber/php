e837a35a6a6eccccce457bb6adac438f5d9d5396
Usman <umahmood@gmail.com>
2020-12-28 23:04:32 +0500

    Add files via upload

---
+++ php-variables.php
@@ -0,0 +1,17 @@
+<?php
+$txt = "Hello World"; // string variable
+$x = 10;  // integer variable
+$y = 9.5; // float variable
+
+// output string variable
+$txt = "PHP Variables";
+// echo "I am learning about $txt";
+
+// print out sum of two variables
+
+$x = 4;
+$y = 5;
+echo $x + $y;
+
+
+?>
