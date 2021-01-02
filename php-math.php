c0e001739656b9f931511b6dc3ef233a71f560e5
Usman <umahmood@gmail.com>
2021-01-02 22:45:34 +0500

    Add files via upload

---
+++ php-math.php
@@ -0,0 +1,32 @@
+<?php
+
+# print the value of pi
+// echo pi();
+
+# using min() and max() functions
+
+// echo min(0,150,30,20,-8,-200); // return min value.
+// echo "<br/>";
+// echo max(0,150,30,20,-8,-200); // return max value
+
+// abs() function to return absolute positive value of function
+
+// echo abs(-6.7);
+
+// using sqrt() function to get square root of a number
+// echo sqrt(64);
+
+// using round() function to return a floating point number
+// to its closed integer
+
+// echo round(0.60);
+// echo round(0.50);
+
+// echo rand();
+
+echo rand(10,100);
+
+
+
+
+?>
