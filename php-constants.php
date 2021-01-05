0092dd78a8e8ae92c9b8b8af8519ee9702f6bb01
Usman <umahmood@gmail.com>
2021-01-05 23:42:34 +0500

    Add files via upload

---
+++ php-constants.php
@@ -0,0 +1,20 @@
+<?php
+
+// define("GREETING","Welcome to W3Schools!", true);
+// echo greeting;
+
+// define("cars",[
+// 	"Alfa Romeo",
+// 	"BMW",
+// 	"Toyota"
+// ]);
+
+// echo cars[2];
+
+define("GREETING","Welcome to W3Schools!", true);
+
+function myTest(){
+	echo GREETING;
+}
+
+myTest();
