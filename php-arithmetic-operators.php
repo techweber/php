e3cd0cbd18417dd48f6e5141750c0b5e36f35e3e
Usman <umahmood@gmail.com>
2021-01-07 00:00:24 +0500

    Add files via upload

---
+++ php-arithmetic-operators.php
@@ -0,0 +1,24 @@
+<?php
+
+$x = 5;
+$y = 8;
+
+# Addition operator
+// $z = $x + $y;
+
+# Subtraction operator
+// $z = $x - $y;
+
+# Multiplication operator
+// $z = $x * $y;
+
+# Division operator
+// $z = $x / $y;
+
+# Modulus operator
+// $z = $x % $y;
+
+# Exponentiation operator
+$z = $x ** $y;
+
+echo $z;
---
+++ php-comparison-operators.php
@@ -0,0 +1,18 @@
+<?php
+
+// $x = 10;
+// $y = "10";
+
+// // if($x === $y) {
+// // 	echo 'x and y are equal';
+// // } else {
+// // 	echo 'x and y are not equal';	
+// // }
+
+// echo $x++;
+
+$x = "Apple";
+$y = "Banana";
+
+$y .= $x;
+echo $y;
