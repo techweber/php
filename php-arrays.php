7253688c18dc5a06df6fe99bf7448bf23f91e626
Usman <umahmood@gmail.com>
2021-01-16 00:13:36 +0500

    Add files via upload

---
+++ php-arrays.php
@@ -0,0 +1,12 @@
+<?php
+
+$cars = array("Volvo","BMW","Toyota");
+
+// echo "Length of cars is " . count($cars);
+
+$person = array("Name"=>"John","Age"=>35);
+
+echo $person['Name'] . ' is ' . $person['Age'];
+
+// echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
